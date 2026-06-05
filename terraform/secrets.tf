c6f9790a56b5cb64236b39f2f953cd57568b930f
irenesoher <irenesoher24@gmail.com>
2026-06-04 20:05:59 -0600

    Update secrets.tf
    agregar codigo de ejemplo

--- terraform/secrets.tf
+++ terraform/secrets.tf
@@ -1,1 +1,23 @@
+resource "google_secret_manager_secret" "meta_verify_token" {
+  secret_id = "meta-verify-token"
+  project   = var.project_id
+  replication {
+    auto {}
+  }
+}
 
+resource "google_secret_manager_secret" "meta_page_token" {
+  secret_id = "meta-page-access-token"
+  project   = var.project_id
+  replication {
+    auto {}
+  }
+}
+
+resource "google_secret_manager_secret" "meta_app_secret" {
+  secret_id = "meta-app-secret"
+  project   = var.project_id
+  replication {
+    auto {}
+  }
+}
