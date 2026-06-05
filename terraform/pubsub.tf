6b2000e3d881bd89b494e15da8efd7055d91e915
irenesoher <irenesoher24@gmail.com>
2026-06-04 20:04:52 -0600

    Update pubsub.tf
    agregar codigo

--- terraform/pubsub.tf
+++ terraform/pubsub.tf
@@ -1,1 +1,5 @@
-
+resource "google_pubsub_topic" "webhook_events" {
+  name                       = "meta-webhook-events"
+  project                    = var.project_id
+  message_retention_duration = "600s"  # 10 minutos
+}
